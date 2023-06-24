da6081783567ffd00d92952df593906006875b60
email <tarunmehto71@gmail.com>
2023-06-24 12:17:07 +0530

    work on product page

--- frontend/javascript/product.js
+++ frontend/javascript/product.js
@@ -1,45 +1,72 @@
 let offerdiv = document.querySelector(".offerdiv");
 offerdiv.style.display = "none";
 
+let price = document.querySelector(".price");
+price.style.display = "none";
+
+let brand = document.querySelector(".brand");
+brand.style.display = "none";
+
+
+
+let size = document.querySelector(".size");
+size.style.display = "none";
+
 let discountdiv = document.querySelector(".discountdiv");
 discountdiv.style.display = "none";
+
+let categorydiv = document.querySelector(".categorydiv");
+categorydiv.style.display = "none";
+
+let sizeRange = document.querySelector(".sizeRange");
+sizeRange.style.display = "none";
+
+
+
+let department = document.querySelector(".department");
+department.style.display = "none";
+
+ let catObj = {
+    0: offerdiv,
+    1: price,
+    2:brand,
+    3 :size,
+    4:discountdiv,
+    5: categorydiv,
+    6 :sizeRange,
+    7:department
+  }
+
 
 let clickLabel = document.querySelectorAll(".clickLabel");
 
 let x = true;
 clickLabel.forEach((item,index)=>{
 
+  // let a = [offerdiv]
+ 
+
+  // console.log(catObj)
 
 item.addEventListener("click", () => {
   console.log(index);
-if(index==0){
+
 
 
   if (x) {
     
-    offerdiv.style.display = "block";
+    catObj[index].style.display = "block";
 
     x = false;
   } else {
-    offerdiv.style.display = "none";
+catObj[index].style.display = "none";
     console.log(item);
     x = true;
   }
 
-}else if(index==1){
-    if (x) {
-    
-        discountdiv.style.display = "block";
-    
-        console.log(item + "a");
-    
-        x = false;
-      } else {
-        discountdiv.style.display = "none";
-        console.log(item);
-        x = true;
-      } 
-}
+
+
+
 
 });
 })
